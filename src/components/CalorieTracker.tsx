a0311a73344c573a9d58c892a263713c28da8182
Stan <moscarstanley37@gmail.com>
2024-10-05 20:21:22 -0600

    created CalorieDisplay component which allows to show different data according to the props passed to it

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,12 +1,14 @@
 import { useMemo } from "react";
 import { Activity } from "../types/types";
+import CalorieDisplay from "./CalorieDisplay";
 
 type CalorieTrackerProps = {
   activities: Activity[];
 };
 
 export const CalorieTracker = ({ activities }: CalorieTrackerProps) => {
-  const caloriesConsumed = useMemo(
+  
+    const caloriesConsumed = useMemo(
     () =>
       activities.reduce(
         (total, activity) =>
@@ -16,6 +18,17 @@ export const CalorieTracker = ({ activities }: CalorieTrackerProps) => {
     [activities]
   );
 
+  const caloriesBurned = useMemo(
+    () =>
+      activities.reduce(
+        (total, activity) =>
+          activity.cathegory === 2 ? total + activity.calories : total,
+        0
+      ),
+    [activities]
+  );
+
+
   return (
     <>
       <h2 className="text-4xl font-black text-white text-center">
@@ -23,13 +36,15 @@ export const CalorieTracker = ({ activities }: CalorieTrackerProps) => {
       </h2>
 
       <div className="flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10">
-        <p className="text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center">
-          <span className="font-black text-6xl text-orange">
-            {" "}
-            {caloriesConsumed}
-          </span>
-            Consumidas  
-        </p>
+
+        <CalorieDisplay
+            calories={caloriesConsumed}
+            text="Consumidas"
+        />
+        <CalorieDisplay
+            calories={caloriesBurned}
+            text="Ejercicio"
+        />
       </div>
     </>
   );
